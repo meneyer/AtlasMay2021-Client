ddd605358659ad2ecb89c84148e74dfe495e2905
Daniel Buergler <daniel.jbuergler@gmail.com>
2021-05-23 22:20:07 -0400

    added chart install and table

--- src/components/admin/AdminIndex.js
+++ src/components/admin/AdminIndex.js
@@ -1,7 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import {Table} from 'reactstrap';
+
+
 const AdminIndex=(props)=>{
     return(
         <div>
-            Admin Index
+            <Table hover id= "formBackground">
+      <thead>
+        <tr>
+        <th>#</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Username</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+        </tr>
+    </tbody>
+    </Table>
+
         </div>
     )
 }
