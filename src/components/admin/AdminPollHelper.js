d6dc900c67bded7c3db9764badf2e0e7d6862bc8
johnulbright <john.ulbright@gmail.com>
2021-06-01 20:43:03 -0400

    fixes adminpoll results

--- src/components/admin/AdminPollHelper.js
+++ src/components/admin/AdminPollHelper.js
@@ -1,13 +1,21 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Button, Table } from "reactstrap";
+import { Bar, Doughnut } from "react-chartjs-2";
+import PollResults from "../poll/PollResults";
 import APIURL from "../../helpers/environment";
+import Poll from "../poll/Poll";
+import AdminPollHelper from "./AdminPollHelper";
 
-const AdminPollHelper = (props) => {
-  const [options, setOptions] = useState([]);
+const AdminPollResults = (props) => {
+  //   const [options, setOptions] = useState([]);
+  //   let [colorList, setColorList] = useState([]);
+  const [polls, setPolls] = useState([]);
+  const [allOptions, setAllOptions] = useState([]);
+  //   const [pollData, setPollData] = useState([]);
 
-  const getOptions = async (poll) => {
-    console.log(poll);
-    const url = `${APIURL}/option/${poll.id}`;
+  const getPolls = async () => {
+    // console.log(props.sessionToken);
+    const url = `${APIURL}/poll/`;
     const result = await fetch(url, {
       method: "GET",
       headers: new Headers({
@@ -17,29 +25,73 @@ const AdminPollHelper = (props) => {
     });
     if (result.status === 200) {
       const res = await result.json();
-      setOptions(res);
+      // res.map((pollData)=>{
+      setPolls(res);
+      // getOptions(pollData.id)
+      console.log("res", res);
+      // })
     } else {
-      console.log(`Failed option fetch: ${result.err}`);
+      console.log(`Failed poll fetch: ${result.err}`);
     }
   };
+
+ 
+
+  const deletePoll = (poll) => {
+    console.log(props.sessionToken);
+    const url = `${APIURL}/poll/${poll.id}`;
+    console.log("got to here in fetch");
+    fetch(url, {
+      method: "DELETE",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: props.sessionToken,
+      }),
+    }).then(() => getPolls());
+  };
+
   useEffect(() => {
-    getOptions(props.poll);
+    getPolls();
   }, []);
 
+  const changeActive=async (poll) => {
+      console.log(poll);
+    const url = `${APIURL}/poll/${poll.id}`;
+    const result = await fetch(url,{
+        method:"PUT",
+        body: JSON.stringify({
+            question: poll.question,
+            published: !poll.published,
+            multiSelect: poll.multiSelect
+          }),
+        headers:new Headers({
+            "Content-Type":"application/json",
+            Authorization:props.sessionToken
+        })
+    })
+    .then(()=>getPolls())
+  }
   return (
-    <Table >
+    <Table style={{backgroundColor:"#C0C0C0"}}>
       <thead>
         <tr>
-          <th>Option</th>
-          <th># of Votes</th>
+          <th>Poll question</th>
+          <th>Results</th>
+          <th>Active?</th>
+          <th>Delete</th>
         </tr>
       </thead>
       <tbody>
-        {options.map((option) => {
+        {polls?.map((poll) => {
           return (
-            <tr>
-              <td>{option.text}</td>
-              <td>{option.votes}</td>
+            <tr key={poll.id}>
+              <td>{poll.question}</td>
+              <td>
+                  <AdminPollHelper sessionToken={props.sessionToken} poll={poll}/>
+                  
+              </td>
+              <td><Button onClick={()=>changeActive(poll)}>{poll.published?"Yes":"No"}</Button></td>
+              <td><Button onClick={()=>deletePoll(poll)}>Delete</Button></td>
             </tr>
           );
         })}
@@ -47,4 +99,5 @@ const AdminPollHelper = (props) => {
     </Table>
   );
 };
-export default AdminPollHelper;
+
+export default AdminPollResults;
